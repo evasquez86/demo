631d3fa2ea68eb8ffc8194c7182e8b870260cf95
Elvis Vasquez <elvasquezsilva@gmail.com>
2018-07-10 02:48:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Registro de Usuario</h1>
+
+    <form>
+          <div class="row">
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p>
+    <input id="email" type="email" class="validate" >
+    </div>
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p>
+    <input id="email" type="email" class="validate">
+    </div>
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p>
+    <input id="email" type="email" class="validate"></div>
+    <div class="col s12 m6 l3"><p>s12 m6 l3</p>
+    <input id="email" type="email" class="validate">
+    </div>
+  </div>
+    </form>
+
+     <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
